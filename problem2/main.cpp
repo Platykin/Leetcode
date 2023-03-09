8c9f1f23a1b8340be09aca06e90a6adf9be3c38f
Igor Alves <igor.alves.pinto11@gmail.com>
2023-03-09 14:55:16 -0300

    First try, not working yet

--- problem2/main.cpp
+++ problem2/main.cpp
@@ -1,18 +1,28 @@
-//I think, solving is the easy part. For the inputs "l1 = [2, 4, 3], l2 = [5, 6, 4]" we could simply multiply each element by a power of 10, for exemplo: l1 = [2, 4, 3] -> l1[i] * 10**i + l1[i] * 10**i + l1[i] * 10**i for each for loop
-
-#include <vector>
-#include <cmath>
 #include <iostream>
+#include <string>
+#include <math.h>
 
-int main(){
-    std::vector<int> l1{2, 4, 3};
-    int sum1{0};
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+  };
 
-    for (int i{0}; i < l1.size(); i++){
-        sum1 += l1[i] * pow(10, i); 
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        int sum1{0}, sum2{0};
+        for (int i{0}; i < l1.size(); i++){
+            sum1 += l1[i] * pow(10, i); 
+        }
+        for (int i{0}; i < l2.size(); i++){
+            sum2 += l1[i] * pow(10, i); 
+        }
+        int res{sum1 + sum2};    
+        std::to_string(res);
+        
     }
-
-    std::cout << sum1 << std::endl;
-
-
-}
+};
