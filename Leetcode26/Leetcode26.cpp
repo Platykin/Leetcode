36b65c3d1c1ea21320b2a88b7b60082035039779
Platykin <igor.alves.pinto11@gmail.com>
2023-08-05 10:42:39 -0300

    done

--- Leetcode26/Leetcode26.cpp
+++ Leetcode26/Leetcode26.cpp
@@ -2,12 +2,12 @@
 #include <vector>
 
 int removeDuplicates(std::vector<int>& nums) {
-    int k = 0;
-    for(int i = 0; i < nums.size(); i++){
-        if (i + 1 != nums.size() && nums[i] == nums[i + 1]){
-            nums.erase(nums.begin()+i+1);
+    int k = 1;
+    for(int i = 1; i < nums.size(); i++){
+        if (nums[i-1] != nums[i]){
+            nums[k] = nums[i];
             k++;
-        }
+        } 
     }
     return k;
 }
