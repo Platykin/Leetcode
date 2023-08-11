2bd055ea0ecc43d0b28cac6aa2f37c548d12f423
Platykin <igor.alves.pinto11@gmail.com>
2023-08-11 20:13:45 -0300

    First easy problem without help!!

--- problem27/problem27.cpp
+++ problem27/problem27.cpp
@@ -1,8 +1,33 @@
 #include <iostream>
+#include <vector>
+
+int removeElement(std::vector<int>& nums, int val) {
+    for(int i = 0; i < nums.size(); i++){
+        if(i+1 != nums.size() && nums[i] == val && nums[i] == nums[i+1]){
+            nums.erase(nums.begin()+i);
+            nums.erase(nums.begin()+i);
+        }
+        if (nums[i] == val){
+            nums.erase(nums.begin()+i);
+        }
+    }
+    return nums.size();    
+}
 
 int main(){
+    std::vector<int> nums = {3, 3};
+    for(int i = 0; i<nums.size(); i++){
+        std::cout << nums[i];
+    }
 
-    std::cout << "Hello World!" << std::endl;
+    std::cout << "\n\n\n";
+
+    removeElement(nums, 2);
+
+    for(int i = 0; i<nums.size(); i++){
+        std::cout << nums[i];
+    }
+
 
     return 0;
 }
